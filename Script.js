db0b69572bf505c3bbb7c9c05571be75271db0da
Yunes Saghoori <yunes2009bob@gmail.com>
2024-03-18 02:44:37 +0330

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,5 +1,26 @@
-var Menu = document.querySelector(".Logo-Menu")
-var Menu2 = document.querySelector(".Menu2")
-Menu.addEventListener("click" , ()=>{
-    document.querySelector(".Menu").ClassList.add(Menu2)
+var open = document.querySelector(".Logo-Menu")
+var Menu = document.querySelector(".Menu2")
+var Snappp = document.querySelector(".Snappp")
+var Close = document.querySelector(".Close");
+Close.style.display = "none";
+Close.style.fontSize = "26px"
+
+open.addEventListener("click" , ()=>{
+    Menu.classList.remove("Menu2");
+    Menu.classList.add("MenuOpen");
+    Menu.id = "";
+    Snappp.classList.remove("Snappp");
+    Snappp.classList.add("Snapppp");
+    open.style.display = "none";
+    Close.style.display = "block";
+})
+
+Close.addEventListener("click" , ()=> {
+    Menu.classList.remove("MenuOpen");
+    Menu.classList.add("Menu2");
+    Menu.id = "Menu2";
+    Snappp.classList.remove("Snapppp");
+    Snappp.classList.add("Snappp");
+    Close.style.display = "none";
+    open.style.display = "block";
 })
