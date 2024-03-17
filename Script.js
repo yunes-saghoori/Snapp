725f52a4e979b068a814662e5930dd75dd7784b0
-ꪗꪊꪀꫀs <142446877+yunes-saghoori@users.noreply.github.com>
2024-03-18 01:32:42 +0330

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+var Menu = document.querySelector(".Logo-Menu")
+var Menu2 = document.querySelector(".Menu2")
+Menu.addEventListener("click" , ()=>{
+    document.querySelector(".Menu").ClassList.add(Menu2)
+})
